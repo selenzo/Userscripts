e30a7ccaa50d11550f628c53d783255dc4675fb8
x-dev <aprogramus@gmail.com>
2016-08-12 16:32:31 +0300

    init

---
+++ onliner.user.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         onliner.by
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       selenzo
+// @match        https://www.onliner.by/*
+// @grant        none
+// ==/UserScript==
+
+(function () {
+    'use strict';
+
+
+})();
