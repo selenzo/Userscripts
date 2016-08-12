9c6cc838aa0b5ad05574f97acfa91bd2ba69eaf8
x-dev <aprogramus@gmail.com>
2016-08-12 16:42:41 +0300

    opt

--- onliner.user.js
+++ onliner.user.js
@@ -1,13 +1,10 @@
 // ==UserScript==
 // @name         onliner.by
-// @namespace    http://tampermonkey.net/
 // @version      0.2
 // @author       selenzo
 // @match        https://*.onliner.by/*
 // @updateURL    https://github.com/selenzo/Userscripts/raw/master/onliner.user.js
-// @grant        none
 // ==/UserScript==
-
 (function () {
     'use strict';
     console.info('userscript!');
