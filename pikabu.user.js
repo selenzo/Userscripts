974ac373d795686ba87b581ae3152c429fb7bccb
x-dev <aprogramus@gmail.com>
2016-08-26 17:19:20 +0300

    ad

--- pikabu.user.js
+++ pikabu.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         pikabu.ru
 // @description  Remove strange
-// @version      0.1
+// @version      0.2
 // @author       selenzo
 // @match        *://pikabu.ru/*
 // @updateURL    https://github.com/selenzo/Userscripts/raw/master/pikabu.user.js
@@ -11,4 +11,5 @@
     console.info('userscript!');
     document.getElementsByClassName("header-cont")[0].getElementsByTagName('a')[0].remove();
     document.getElementsByClassName("menu-block-branding")[0].getElementsByTagName('a')[0].remove();
+    document.getElementById("branding_ad_footer").parentElement.remove();
 })();
